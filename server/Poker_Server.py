eb139b5e23fb059926a4521c937a49cd09c6730e
Aditya Jain <adityajain080601@gmail.com>
2021-07-06 17:04:53 +0530

    Server can now listen on socket

--- server/Poker_Server.py
+++ server/Poker_Server.py
@@ -12,18 +12,63 @@ class Server:
 
 	def start(self):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 		
+		try:
+			self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+		except Exception as e:
+			print(f"Socket exception on SO_REUSEPORT {str(e)}")
+			return False
+		
+		print(f"Binding to port {self.port}")
+		try:
+			self.sock.bind(('', self.port))
+		except socket.error as e:
+			print(f"Error while binding {str(e)}")
+			return False
+
+		try:
+			self.sock.setblocking(False)
+		except socket.error as e:
+			print(f"failed to set non-blocking {e}")
+			return False
+
+		try:
+			self.sock.listen(3)
+		except socket.error as e:
+			print(f"Failed to listen with error {str(e)}")
+			return False
+
 		self.thread = threading.Thread(target=self.run)
 		self.thread.daemon = True
 		self.thread.start()
-
-	def run(self):
-		for _ in range(10):
-			print("Hello from thread function")
+		return True
 	
 	def stop(self):
-		pass
+		try:
+			self.sock.shutdown(socket.SHUT_RDWR)
+		except socket.error as e:
+			print(f"Failed to shutdown server with error {str(e)}")
+		
+
+	def run(self):
+		inputs = [self.sock]
+		outputs = []
+		while inputs:
+			readable, _, excetional = select.select(inputs, outputs, inputs)
+
+			for sock in readable:
+				if sock in self.sock: # new client
+					client_sock, client_address = self.sock.accept()
+					try:
+						client_sock.setblocking(False)
+					except socket.error as e:
+						print(f"Failed to set client socket to non-blocking address:{client_address}, error:{str(e)}")
+						return False
+					inputs.append(client_sock)
+					self.onConnect(client_sock)
+				else: # msg from already connected client
+					pass
+
 
 	def onConnect(self):
 		pass
@@ -40,8 +85,10 @@ class Server:
 
 
 if __name__ == '__main__':
-	port = 1234
+	port = 12345
 	PServer = Server(port)
 	PServer.start()
+	while True:
+		sleep(10)
 
 # %%
