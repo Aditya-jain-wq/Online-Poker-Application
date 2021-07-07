22f29683c36feba9dec7d4fcb58abc16132dde78
Kushagra Gupta <kushgpt08@gmail.com>
2021-07-07 15:26:59 +0530

    some base setup

---
+++ server/room.py
@@ -0,0 +1,5 @@
+MAX_PLAYERS_IN_GAME = 8
+
+
+class Room:
+    pass
--- server/server.py
+++ server/server.py
@@ -1,92 +1,49 @@
-import select
-import socket
-import threading
-import logging
-from time import sleep
+# import select
+# import socket
+# import threading
+# import logging
+# from time import sleep
+
+from socketserver import TCPServer, BaseRequestHandler
+import json
+from typing import Dict, Tuple
+
+from commands import *
+from room import Room, MAX_PLAYERS_IN_GAME as MPIG
 
 
-class Server:
-    def __init__(self, port):
-        self.thread = None
-        self.port = int(port)
-        self.conn_sock = set()
+class PokerHandler(BaseRequestHandler):
+    def __init__(self, rooms: Dict[str, Room]):
+        self.rooms = rooms
 
-    def start(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        try:
-            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        except Exception as e:
-            print(f"Socket exception on SO_REUSEPORT {str(e)}")
-            return False
-
-        print(f"Binding to port {self.port}")
-        try:
-            self.sock.bind(("", self.port))
-        except socket.error as e:
-            print(f"Error while binding {str(e)}")
-            return False
-
-        try:
-            self.sock.setblocking(False)
-        except socket.error as e:
-            print(f"failed to set non-blocking {e}")
-            return False
-
-        try:
-            self.sock.listen(3)
-        except socket.error as e:
-            print(f"Failed to listen with error {str(e)}")
-            return False
-
-        self.thread = threading.Thread(target=self.run)
-        self.thread.daemon = True
-        self.thread.start()
-        return True
-
-    def stop(self):
-        try:
-            self.sock.shutdown(socket.SHUT_RDWR)
-        except socket.error as e:
-            print(f"Failed to shutdown server with error {str(e)}")
-
-    def run(self):
-        inputs = [self.sock]
-        outputs = []
-        while inputs:
-            readable, _, excetional = select.select(inputs, outputs, inputs)
-
-            for sock in readable:
-                if sock in self.sock:  # new client
-                    client_sock, client_address = self.sock.accept()
-                    try:
-                        client_sock.setblocking(False)
-                    except socket.error as e:
-                        print(
-                            f"Failed to set client socket to non-blocking address:{client_address}, error:{str(e)}"
-                        )
-                        return False
-                    inputs.append(client_sock)
-                    self.onConnect(client_sock)
-                else:  # msg from already connected client
-                    pass
-
-    def onConnect(self, sock):
-        self.conn_sock.add(sock)
-
-    def onDisconnect(self):
-        pass
-
-    def onMsgReceive(self):
-        pass
-
-    def sendMsg(self):
-        pass
+    def handle(self):
+        self.data = self.request.recv(16192).strip()
+        payload = json.loads(self.data)
+        if payload["kind"] == "JOIN":
+            cmd = JoinCmd(payload["payload"])
+            assert (
+                len(self.rooms[cmd.room].players) < MPIG
+                and not self.rooms[cmd.room].started
+            )
+            self.rooms[cmd.room].players.append((self.client_address, cmd.username))
+        elif payload["kind"] == "CREATE":
+            cmd = CreateCmd(payload["payload"])
+            pass
+        else:
+            cmd = {
+                "RAISE": RaiseCmd,
+                "FOLD": FoldCmd,
+            }[payload["kind"]]
+            cmd = cmd(**payload["cmd"])
+            room = cmd.room
+            self.rooms[room] = self.rooms[room].update(cmd)
+            # self.request.sendall(self.rooms[room])
+        # self.request.sendall(self.data.upper())
 
 
 if __name__ == "__main__":
-    port = 12345
-    PServer = Server(port)
-    PServer.start()
-    while True:
-        sleep(10)
+    PORT = 12345
+    rooms = {}
+
+    with TCPServer(("", PORT), PokerHandler(rooms)) as server:
+        server.serve_forever()
